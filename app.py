0af8dce85a51e0b755c348cd8dd39312a957544b
Brian Zhang <brian22ca@gmail.com>
2021-07-27 11:37:21 -0400

    Huge changes. Added login/registration system, added TODO list, and many many other small changes.

--- app.py
+++ app.py
@@ -1,37 +1,144 @@
-from flask import Flask, render_template, request, redirect
+from sqlite3.dbapi2 import IntegrityError
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "secret secret spook"
 
-con = sqlite3.connect("rt.db")
+# Create table if it doesn't exist
+con = sqlite3.connect("data.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS times (id INTEGER, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, rt INTEGER, pi INTEGER, PRIMARY KEY(id));")
+cur.execute("""CREATE TABLE IF NOT EXISTS users (
+    id INTEGER,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id))
+    """)
+cur.execute("""CREATE TABLE IF NOT EXISTS rts (
+    id INTEGER,
+    uid INTEGER NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    rt INTEGER NOT NULL,
+    pi INTEGER NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (uid) REFERENCES users(id));
+""")
 con.commit()
+cur.close()
 con.close()
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.route("/")
 def index():
+    return redirect("/test")
+
+
+@app.route("/test", methods=["GET", "POST"])
+def test():
+    if not session.get("logged_in"):
+        session["logged_in"] = False
+
     if request.method == "GET":
-        return render_template("index.html")
-    rt = request.form.get("rt");
-    pi = request.form.get("pi")
-    if rt == None:
+        logged_in = session["logged_in"]
+        return render_template("test.html", logged_in=logged_in)
+    elif request.method == "POST":
+        rt = request.form.get("rt")
+        pi = request.form.get("pi")
+        con = sqlite3.connect("data.db")
+        cur = con.cursor()
+        cur.execute("INSERT INTO rts (uid, rt, pi) VALUES (?, ?, ?)", (session["uid"], rt, pi))
+        con.commit()
+        cur.close()
+        con.close()
         return redirect("/")
-    con = sqlite3.connect("rt.db")
-    cur = con.cursor()
-    cur.execute("INSERT INTO times (rt, pi) VALUES (?, ?)", (rt, pi))
-    con.commit()
-    con.close()
-    return redirect("/")
+
 
 @app.route("/view", methods=["GET", "POST"])
 def view():
-    con = sqlite3.connect("rt.db")
+    con = sqlite3.connect("data.db")
     cur = con.cursor()
+    cur.execute("""
+        SELECT * FROM rts
+        JOIN users ON users.id = rts.uid
+        """)
     if (request.method == "GET"):
-        return render_template("view.html", db=cur.execute("SELECT * FROM times"))
-    row_id = request.form.get("id")
-    cur.execute("DELETE FROM times WHERE id=?", (row_id,))
-    con.commit()
-    con.close()
-    return redirect("/view")
+        return render_template("view.html", db=cur.fetchall())
+    elif (request.method == "POST"):
+        row_id = request.form.get("id")
+        cur.execute("DELETE FROM rts WHERE id=?", (row_id,))
+        con.commit()
+        cur.close()
+        con.close()
+        return redirect("/view")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        if not session.get("logged_in"):
+            session["logged_in"] = False
+        
+        if session["logged_in"]:
+            return "Already Logged In!"
+        else:
+            return render_template("login.html")
+    elif request.method == "POST":
+        usernameInput = request.form.get("username")
+        passwordInput = request.form.get("password")
+        con = sqlite3.connect("data.db")
+        cur = con.cursor()
+        cur.execute("SELECT password FROM users WHERE username = ?", (usernameInput,))
+        password = cur.fetchone()
+        if password:
+            password = password[0]
+        else:
+            return render_template("login.html", failed=True)
+        if password and password == passwordInput:
+            cur.execute("SELECT id FROM users WHERE username = ?", (usernameInput,))
+            uid = cur.fetchone()[0]
+            session["logged_in"] = True
+            session["uid"] = uid
+        else:
+            return render_template("login.html", failed=True)
+        cur.close()
+        con.close()
+        return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if (request.method == "GET"):
+        if not session.get("logged_in"):
+            session["logged_in"] = False
+        
+        if session["logged_in"]:
+            return "Already Logged In!"
+        else:
+            return render_template("register.html")
+    
+    elif (request.method == "POST"):
+        username = request.form.get("username")
+        password = request.form.get("password")
+        con = sqlite3.connect("data.db")
+        cur = con.cursor()
+
+        try:
+            cur.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+        except IntegrityError:
+            return render_template("register.html", usernameTaken=True)
+        cur.execute("SELECT id FROM users WHERE username = ?", (username,))
+        uid = cur.fetchone()[0]
+        con.commit()
+        cur.close()
+        con.close()
+        session["logged_in"] = True
+        session["uid"] = uid
+        return redirect("/")
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    if session["logged_in"]:
+        session["logged_in"] = False
+        session.pop("uid")
+        return render_template("logout.html", logged_in=True)
+    elif not session["logged_in"]:
+        return render_template("logout.html", logged_in=False)
---
+++ static/test.js
@@ -0,0 +1,82 @@
+document.onkeydown = (e) => {
+    if (e.key == " ") {
+        document.onkeydown = null;
+        startTest();
+    } else if (e.key == "v") {
+        let form = document.createElement("form");
+        document.body.appendChild(form);
+        form.action = "/view";
+        form.method = "get";
+        form.submit();
+    }
+}
+
+class ReactionTimer {
+    constructor(endTest) {
+        this.pi = (Math.floor(Math.random() * 301) + 100) * 10;
+        this.timeElapsed = 0;
+        this.endTest = endTest;
+    }
+    startDelayTimer() {
+        document.onkeydown = (e) => {
+            if (e.key == " ") {
+                document.onkeydown = null;
+                document.querySelector("#skipResultsForm").submit();
+            }
+        }
+        this.timer = setInterval(this.calculateDelayTime.bind(this), 10);
+    }
+    calculateDelayTime() {
+        this.timeElapsed += 10;
+        if (this.timeElapsed >= this.pi) {
+            clearInterval(this.timer);
+            this.timeElapsed = 0;
+            this.startTime = new Date();
+            document.querySelector("#content").style.backgroundColor = "green";
+            document.onkeydown = (event) => {
+                if (event.key == " ") {
+                    let end = new Date();
+                    document.onkeydown = null;
+                    let rt = end - this.startTime;
+                    this.endTest(rt, this.pi);
+                }
+            };
+        }
+    }
+}
+
+function startTest() {
+    startButton = document.querySelector(".startButton");
+    viewText = document.querySelector(".viewText");
+    startButton.style.display = "none";
+    viewText.style.display = "none";
+    let timer = new ReactionTimer(endTest);
+    timer.startDelayTimer();
+}
+
+function endTest(rt, pi) {
+    document.onkeydown = (event) => {
+        if (event.key == " ") {
+            if (logged_in) {
+                // Insert values and submit form
+                let form = document.querySelector("#submitResultsForm");
+                let rtTag = document.querySelector("#rtTag");
+                rtTag.value = rt;
+                let piTag = document.querySelector("#piTag");
+                piTag.value = pi;
+                form.submit();
+            }
+            else {
+                document.querySelector("#loginForm").submit();
+            }
+
+        } else if (event.key == "d") {
+            document.querySelector("#skipResultsForm").submit();
+        }
+    };
+    let results = "You Reacted in: " + rt + " ms!";
+    let resultsElement = document.querySelector("#results");
+    let resultsTextElement = document.querySelector("#resultsText");
+    resultsTextElement.innerHTML = results;
+    resultsElement.style.display = "flex";
+}
